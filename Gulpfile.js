86f1b43c7a762b0cf7e2f3ca375b8e93e889fa64
Felipe Lopes <felipe.webdev@gmail.com>
2015-12-17 15:23:27 -0300

    first commit

---
+++ Gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')({port: 35729}));
+  app.use(express.static(__dirname));
+  app.listen(4000, '0.0.0.0');
+});
+
+gulp.task('default', ['express', 'watch', 'livereload'], function() {
+
+});
+
+// Sass Task
+gulp.task('styles', function() {
+  return sass('sass', { style: 'expanded' })
+    .pipe(gulp.dest('css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'));
+});
+
+// Gulp Watch Task
+gulp.task('watch', function() {
+  gulp.watch('sass/*.scss', ['styles']);
+});
+
+// Live Reload Task
+var tinylr;
+gulp.task('livereload', function() {
+  tinylr = require('tiny-lr')();
+  tinylr.listen(35729);
+});
+
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
